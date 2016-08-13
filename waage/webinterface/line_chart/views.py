8c34caa0a50c3a3414eda6b0060a58b2dd408da4
Your Name <you@example.com>
2016-08-13 06:42:54 +0000

    httprespone+render

--- waage/webinterface/line_chart/views.py
+++ waage/webinterface/line_chart/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import *
 import json as simplejson
 import sqlite3
@@ -6,26 +7,32 @@ import sqlite3
 
 def line_chart(request):
     if (request.POST.has_key('date_to') and request.POST.has_key('date_from')):
+        date_from=request.POST['date_from']
+        date_to=request.POST['date_to']
+        print('******')
+        print(date_to)
+        print('******')
         response_dict={}
         response_dict.update({'test':'dd'})
         return HttpResponse(simplejson.dumps(response_dict))
-    else:
-        connection = sqlite3.connect("../waage.db")
-        cursor = connection.cursor()
-        sql_command = """
-        SELECT Datum from (SELECT * FROM waage order by ID desc limit 10) order by ID asc
-        ;"""
-        datum = cursor.execute(sql_command)
-        datum = datum.fetchall()
-        sql_command = """
-        SELECT Gewicht from (SELECT * FROM waage order by ID desc limit 10) order by ID asc
-        ;"""
-        messwert = cursor.execute(sql_command)
-        messwert = messwert.fetchall()
-        connection.commit()
-        connection.close()
-
-        return render(request, "line_chart/line_chart.html",{"messwerte":messwert, "datum":datum})
+    connection = sqlite3.connect("../waage.db")
+    cursor = connection.cursor()
+    sql_command = """
+    SELECT Datum from (SELECT * FROM waage order by ID desc limit 10) order by ID asc
+    ;"""
+    datum = cursor.execute(sql_command)
+    datum = datum.fetchall()
+    sql_command = """
+    SELECT Gewicht from (SELECT * FROM waage order by ID desc limit 10) order by ID asc
+    ;"""
+    messwert = cursor.execute(sql_command)
+    messwert = messwert.fetchall()
+    connection.commit()
+    connection.close()
+    t=loader.get_template('line_chart/line_chart.html')
+    c={"messwerte":messwert, "datum":datum}
+    return HttpResponse(t.render(c, request))
+        #return render(request, "line_chart/line_chart.html",{"messwerte":messwert, "datum":datum})
 
 
 def test():
