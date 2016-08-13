699ce583ee39311d52efd079f1b9c42b5339bb55
Your Name <you@example.com>
2016-08-13 11:29:08 +0000

    ajax.url /a/ veraendert, unicode zu string umgewandelt

--- waage/webinterface/line_chart/urls.py
+++ waage/webinterface/line_chart/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^test', views.line_chart, name='line_chart'),
+    url(r'^test/$', views.line_chart, name='line_chart'),
+    url(r'^a/$', views.confirm_ajax, name='confirm_ajax'),
 ]
--- waage/webinterface/line_chart/views.py
+++ waage/webinterface/line_chart/views.py
@@ -5,16 +5,42 @@ import json as simplejson
 import sqlite3
 
 
-def line_chart(request):
+def confirm_ajax(request):
     if (request.POST.has_key('date_to') and request.POST.has_key('date_from')):
         date_from=request.POST['date_from']
+        date_from=date_from.encode('ascii', 'xmlcharrefreplace')
+        print((date_from))
+        data_from_day=date_from[3]
+        data_from_day+=date_from[4]
+        data_from_day=int(data_from_day)
+        data_from_month=date_from[0]
+        data_from_month+=date_from[1]
+        data_from_month=int(data_from_month)
+        data_from_year=date_from[6]
+        data_from_year+=date_from[7]
+        data_from_year=date_from[8]
+        data_from_year+=date_from[9]
+        data_from_year=int(data_from_year)
         date_to=request.POST['date_to']
-        print('******')
-        print(date_to)
-        print('******')
+        data_to_day=date_to[3]
+        data_to_day+=date_to[4]
+        data_to_day=int(data_to_day)
+        data_to_month=date_to[0]
+        data_to_month+=date_to[1]
+        data_to_month=int(data_to_month)
+        data_to_year=date_to[6]
+        data_to_year+=date_to[7]
+        data_to_year+=date_to[8]
+        data_to_year+=date_to[9]
+        data_to_year=int(data_to_year)
+        print(data_to_year)
+
         response_dict={}
         response_dict.update({'test':'dd'})
         return HttpResponse(simplejson.dumps(response_dict))
+    
+
+def line_chart(request):
     connection = sqlite3.connect("../waage.db")
     cursor = connection.cursor()
     sql_command = """
